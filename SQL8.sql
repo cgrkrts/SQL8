db1691fd787c05ba971f7c85f5321f9df3127507
Cagri Kirtis <cagrikirtis17@gmail.com>
2024-03-03 14:48:16 +0300

    first commit

---
+++ SQL8.sql
@@ -0,0 +1,61 @@
+--CREATE TABLE employee (
+--id INTEGER, 
+--name VARCHAR(50), 
+--birthday DATE,
+--email VARCHAR(100)
+--);
+
+
+--UPDATE employee
+--SET name = 'Jony',
+--	birthday = '1984-12-01',
+--	email = 'jony@mail.eu'
+--WHERE name = 'Loni';
+
+
+--UPDATE employee
+--SET name = 'Alex',
+	--birthday = '1990-12-05',
+	--email = 'alex@mail.com'
+--WHERE id = 15;
+
+
+--UPDATE employee
+--SET id = '101',
+--	birthday = '1988-08-08',
+--	email = 'emogene@mail.com'
+--WHERE name = 'Emogene';
+
+
+--UPDATE employee
+--SET id = '67',
+--	birthday = '1987-07-07',
+--	email = 'stavros@mail.com'
+--WHERE id = 66;
+
+
+--UPDATE employee
+--SET id = '50',
+--	name = 'tress',
+--	birthday = '1985-05-05'
+--WHERE email = 'tforesc@army.mil';
+
+
+--DELETE FROM employee
+--WHERE id = 33;
+
+
+--DELETE FROM employee
+--WHERE id = 75;
+
+
+--DELETE FROM employee
+--name = 'Raynard';
+
+
+--DELETE FROM employee
+--WHERE birthday = '1991-07-02';
+
+
+DELETE FROM employee
+WHERE email = 'celvidgef@ftc.gov';
